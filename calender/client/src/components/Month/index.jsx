2952edae7c6e737235c11ad9156b438080f39669
Colton Firestone <colton.firestone@gmail.com>
2024-01-27 14:17:24 -0800

    start integrating with month.jsx

--- calender/client/src/components/Month/index.jsx
+++ calender/client/src/components/Month/index.jsx
@@ -1,8 +1,12 @@
 import classes from './index.module.css';
 import { useState, useEffect } from 'react';
+import DateRangeInfo from '../../utils/dateInfo';
 
 
 const Month = () => {
+
+let testDate = '2024-2-06';
+
 
 let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,23,24,25,26,27,28,29,30];
 let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];  
--- calender/client/src/utils/dateInfo.js
+++ calender/client/src/utils/dateInfo.js
@@ -1,112 +1,132 @@
-//the purpose for this class it to be able to take in a string representation of a date and output the info needed to create the calender in a string format
-// the reason for this is to normalize the data so that it can be used in the calender component and can be used with the database to get the events for the calender
-// it is important to note that for our purposes sunday is the first day of the week and the week starts on sunday and ends on saturday
+//This class provides a comprehensive set of tools for working with dates and ranges of dates, including extracting the year, month, and day from a date string, determining the number of days in a month, generating date strings for each month in a year, and generating a list of dates for the week containing a selected date 
 
 class DateRangeInfo {
-  constructor({selectedDate, range}) {
-    this.selectedDate = selectedDate;
-    this.range = range;
-    this.startDate = null
-    this.daysInMonth = null
-    this.day = null
-    this.month = null
-    this.year = null
-  }
+    constructor({selectedDate, range}) {
+       // Constructor initializes the class with the selected date and range
+       this.selectedDate = selectedDate;
+       this.range = range;
+       this.startDate = null
+       this.daysInMonth = null
+       this.day = null
+       this.month = null
+       this.year = null
+    }
+   
     getStartDate() {
-         this.startDate = new Date(this.selectedDate);
+       // Method to convert the selected date string into a Date object
+       this.startDate = new Date(this.selectedDate);
     } 
+   
     splitDate() {
-        let match = this.selectedDate.match(/(\d{4})-(\d{2})-(\d{2})/);
-        this.year = match[1]; 
-        this.month = match[2]; 
-        this.day = match[3];
-        console.log(`year: ${this.year}, month: ${this.month}, day: ${this.day}`)
+       // Method to extract the year, month, and day from the selected date string
+       let match = this.selectedDate.match(/(\d{4})-(\d{2})-(\d{2})/);
+       this.year = match[1]; 
+       this.month = match[2]; 
+       this.day = match[3];
+       console.log(`year: ${this.year}, month: ${this.month}, day: ${this.day}`)
     }
+   
     establishDateInfo() {
-        this.getStartDate();
-        this.splitDate();
-        this.daysInMonth = this.getDaysInMonth(this.year, this.month);
-        console.log(`there are ${this.daysInMonth} days in the month of ${this.month}`);
+       // Method to initialize the start date, split the date, and get the number of days in the selected month
+       this.getStartDate();
+       this.splitDate();
+       this.daysInMonth = this.getDaysInMonth(this.year, this.month);
+       console.log(`there are ${this.daysInMonth} days in the month of ${this.month}`);
     }
-    // this is the info i need for making the calender output properly
+   
     getMonthsInYear() {
-        // i need to return the start dates of each month as a string 
-        // i need to return the number of days in each month
-        // i need to return the day of the week the first day of the month lands on
-        let months = [];
-        for (let i = 1; i <= 12; i++) {
-            let monthStr = i < 10 ? `0${i}` : `${i}`;
-            let dateString = `${this.year}-${monthStr}-01`;
-            let day = this.getDayOfWeek(new Date(dateString));
-            let daysInMonth = this.getDaysInMonth(this.year, i);
-            months.push({dateString, day, daysInMonth, dateArray: []});
-        }
-        
-        for (let i = 0; i < months.length; i++) {
-            let dates = [];
-            for (let day =1; day <= months[i].daysInMonth; day++) { 
-            let monthStr = i < 10 ? `0${i}` : `${i}`;
-            let dayStr = day < 10 ? `0${day}` : `${day}`;
-            let dateString = `${this.year}-${monthStr}-${dayStr}`;
-            dates.push(dateString);
-            }
-            months[i].dateArray = dates;
-        }
-
-        console.log(months);
-        
+       // Method to generate an array of objects representing each month in the selected year
+       let months = [];
+       for (let i = 1; i <= 12; i++) {
+           let monthStr = i < 10 ? `0${i}` : `${i}`;
+           let dateString = `${this.year}-${monthStr}-01`;
+           let day = this.getDayOfWeek(new Date(dateString));
+           let daysInMonth = this.getDaysInMonth(this.year, i);
+           months.push({dateString, day, daysInMonth, dateArray: []});
+       }
+       return months; 
     }
+   
+    getDatesInMonth() {
+       // Method to populate the dateArray property of each month object with an array of date strings
+       let months = this.getMonthsInYear();
+       for (let i = 0; i < months.length; i++) {
+           let dates = [];
+           for (let day =1; day <= months[i].daysInMonth; day++) { 
+           let monthStr = i < 10 ? `0${i}` : `${i}`;
+           let dayStr = day < 10 ? `0${day}` : `${day}`;
+           let dateString = `${this.year}-${monthStr}-${dayStr}`;
+           dates.push(dateString);
+           }
+           months[i].dateArray = dates;
+       }
+       console.log(months);
+    }
+   
     getDaysInMonth(year, month) {
-        return new Date(year, month, 0).getDate();
+       // Method to calculate the number of days in a given month of a given year
+       return new Date(year, month, 0).getDate();
     }
+   
     getDayOfWeek(date) {
-        return date.getDay(); 
+       // Method to get the day of the week for a given date
+       return date.getDay(); 
     }
-    // this is basically for when someone is on the month level of the calender and they select a certain day 
-    // i need the info for the week that day is in
-    // this will be used to create the week level of the calender
+   
     getWeekInfo() {
-        let dayOfWeek = this.getDayOfWeek(this.startDate);
-        console.log(dayOfWeek);
-        let weekStart = this.day - dayOfWeek - 1;
-        let weekEnd = weekStart + 6;
-        console.log(` *** weekStart: ${weekStart}, weekEnd: ${weekEnd}`);
-        let list = [];
-        for (let i = weekStart; i <= weekEnd; i++) {
-            let month = +this.month ;
-            let days = i;
-            //handles days that fall after end of selected month
-            if (i > this.daysInMonth) {
-                days -= this.daysInMonth;
-                month++;
-            }
-            // handles days that fall before start of selected month
-            if (i < 1) {
-                days += this.getDaysInMonth(this.year, this.month - 1);
-                month--;
-            }
-            let day = days < 10 ? `0${days}` : `${days}`;
-            let monthStr = month < 10  ? `0${month}` : `${month}`;
-            let dateString = `${this.year}-${monthStr}-${day}`;
-            list.push(dateString);
-        }
-        console.log(list);
+       // Method to get the start and end dates of the week containing the selected date
+       let dayOfWeek = this.getDayOfWeek(this.startDate);
+       let weekStart = this.calculateWeekStart(dayOfWeek);
+       let weekEnd = weekStart + 6;
+   
+       let list = this.generateDatesList(weekStart, weekEnd);
+   
+       console.log(list);
+    }
+   
+    calculateWeekStart(dayOfWeek) {
+       // Helper method to calculate the start of the week containing the selected date
+       return this.day - dayOfWeek - 1;
+    }
+   
+    generateDatesList(weekStart, weekEnd) {
+       // Helper method to generate an array of date strings for the week containing the selected date
+       let list = [];
+       for (let i = weekStart; i <= weekEnd; i++) {
+           let month = +this.month;
+           let days = i;
+           if (i > this.daysInMonth) {
+               days -= this.daysInMonth;
+               month++;
+           }
+           if (i < 1) {
+               days += this.getDaysInMonth(this.year, this.month - 1);
+               month--;
+           }
+           let day = days < 10 ? `0${days}` : `${days}`;
+           let monthStr = month < 10 ? `0${month}` : `${month}`;
+           let dateString = `${this.year}-${monthStr}-${day}`;
+           list.push(dateString);
+       }
+       return list;
+    }
+
+    getNameOfDay() {
+        // Method to get the name of the day of the week for a given date
+        let daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"];
+        // Method to get the name of the day of the week for a given date
+        let nameOfDay = daysOfWeek[+this.day];
+        return nameOfDay;
+   }
+
+   getNameOfMonth() {
+         // Method to get the name of the month for a given date
+       let monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October","November","December"];
+       // Method to get the name of the month for a given date
+       let nameOfMonth = monthsOfYear[+this.month + -1];
+       return nameOfMonth;
+   }
+   
 }
-}
 
-let daysOfWeekForTesting = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"];
-
-let testDate = new DateRangeInfo({selectedDate: "2024-10-30", range: 7});
-testDate.establishDateInfo();
-testDate.getWeekInfo();
-
-
-let testDate2 = new DateRangeInfo({selectedDate: "2024-02-02", range: 7});
-testDate2.establishDateInfo();
-testDate2.getWeekInfo();
-
-
-
-// console.log(`the day of the week is ${daysOfWeekForTesting[testDate.getDayOfWeek(testDate.startDate)]}`);
-// // testDate.getMonthsInYear();
-
+export default DateRangeInfo;
