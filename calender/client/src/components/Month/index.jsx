bb76ea74ec7567e68a4ff54c1f4e836a283b609d
Colton Firestone <colton.firestone@gmail.com>
2024-01-27 21:59:14 -0800

    all interactive stuff on month view is good, moving on to the week view

--- calender/client/src/components/Month/index.jsx
+++ calender/client/src/components/Month/index.jsx
@@ -45,28 +45,8 @@ let testEventArray = [{
 
 
 const Month = ({date}) => {
- date = '2024-05-01';
-
-
-let myMonth = new DateRangeInfo({selectedDate: date, range: "week"});
-console.log(myMonth.range);
-console.log(myMonth.selectedDate)
-myMonth.establishDateInfo()
-
-//this part will set the layout of the calender
-console.log(myMonth.day);
-
-let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];  
-let firstDay = myMonth.getNameOfDay();
-let miniDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-
-
-let blanksBeforeDates = Array(days.indexOf(firstDay)).fill(" ");
-let numbers = Array.from({length: myMonth.daysInMonth}, (_, i) => i + 1);
-let combinedArray = [...blanksBeforeDates, ...numbers];
-
-let dateList = myMonth.getDatesInMonth();
-console.log(dateList);
+  //WHEN GENERATING THE MONTH I NEED TO MAKE SURE THAT THE DATE BROUGHT IN IS THE FIRST OF THE MONTH
+ date = '2024-02-01';
 
 // handles resizing of the window
 const [windowSize, setWindowSize] = useState("");
@@ -111,26 +91,88 @@ useEffect(() => {
  }
 }, [windowSize]);
 
-    const squares = Array.from({ length: 42 }).map((_, i) => (
-        <div key={i} className="flex items-end aspect-w-3 aspect-h-3 border border-black">
-         
-          <div className="flex flex-col justify-center h-full">
-              <div className="flex justify-center" >
-              </div>
-              <div className="mt-auto flex justify-center">
-                  <h2>{combinedArray[i]}</h2>
-              </div>
-          </div>
-        </div>
-    ));
+
+let myMonth = new DateRangeInfo({selectedDate: date, range: "week"});
+console.log(myMonth.range);
+console.log(myMonth.selectedDate)
+myMonth.establishDateInfo()
+
+//this part will set the layout of the calender
+console.log(myMonth.day);
+
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];  
+let firstDay = myMonth.getNameOfDay();
+let miniDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+let dateList = myMonth.getDatesInMonth();
+console.log(dateList);
+let monthAtAGlance = [];
+let blanks = days.indexOf(firstDay);
+let blanksArray = Array(blanks).fill({
+  event: false,
+  day: ""
+});
+
+let datesArray=[]
+for (let i = 0; i < dateList.length; i++) {
+  let dateObject;
+  dateObject = {
+    event: false,
+    date: dateList[i],
+    day: + dateList[i].match(/(\d{4})-(\d{2})-(\d{2})/)[3],
+  }
+  // THIS NEEDS TO BE REFACTORED WHEN ACTUAL DATA IS BEING BROUGHT IN!!!!
+  for (let j = 0; j < testEventArray.length; j++) {
+    if (dateList[i] === testEventArray[j].date) {
+      dateObject = {
+        event: true,
+        date: dateList[i],
+        day: +dateList[i].match(/(\d{4})-(\d{2})-(\d{2})/)[3],
+        events: testEventArray[j]
+      }
+    }
+  }
+  datesArray.push(dateObject)
+}
+
+monthAtAGlance = [...blanksArray, ...datesArray];
+
+
+const dateSquares = Array.from({ length: 42 }).map((_, i) => (
+  <div 
+  key={i} 
+  className={`flex items-end aspect-w-3 aspect-h-3 border border-black ${monthAtAGlance[i]?.day && monthAtAGlance[i].day >= 1 ? classes.valid_day : classes.invalid_day} cursor-pointer`}
+  // HEY!!! this is where i need to add the onClick function to go to the week view
+  onClick={() => {
+     if (monthAtAGlance[i]?.day && monthAtAGlance[i].day >= 1) {
+       console.log(monthAtAGlance[i].date);
+     }
+  }}
+>
+     <div className="flex flex-col justify-center h-full">
+         <div className="flex justify-center" >
+           {/* THIS NEEDS TO BE REFACTORED WHEN ACTUAL DATA IS BEING BROUGHT IN!!!! */}
+           {monthAtAGlance[i]?.event ? <div className='w-[calc(3vw)] h-[calc(3vw)] rounded-full mt-1' style={{backgroundColor: 'black'}}></div> : <h3></h3>}
+           {/* */}
+         </div>
+         <div className="mt-auto flex justify-center">
+             <h2>{monthAtAGlance[i]?.day || ''}</h2>
+         </div>
+     </div>
+  </div>
+ ));
+ 
+
+
 
     return (
       <div className="flex flex-col  justify-center mt-10">
+         <h1 className='flex justify-center text-2xl font-thin mb-5'>{myMonth.getNameOfMonth()}</h1>
+        
          <div className="flex flex-row justify-between items-center w-5/6 md:w-3/4 mx-auto">
           {daysDisplay}
          </div>
          <div className="grid grid-cols-7 gap-0 w-5/6 md:w-3/4 mx-auto">
-           {squares}
+           {dateSquares}
          </div>
       </div>
      )
