809859929be8eea861deb1d649dfdda912a8185c
Colton Firestone <colton.firestone@gmail.com>
2024-01-29 23:12:02 -0800

    added some new dependencies

--- calender/client/src/components/Year/index.jsx
+++ calender/client/src/components/Year/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import DateRangeInfo from '../../utils/dateInfo';
 import classes from './index.module.css';
+import { useParams } from "react-router-dom";
 
 let testEventArray = [{
   title: "test event",
