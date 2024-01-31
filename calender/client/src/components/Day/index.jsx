da540cb4ddaccd6b7f20b88d3c8f33b62cdd7ef5
Colton Firestone <colton.firestone@gmail.com>
2024-01-30 22:43:19 -0800

    established day page

---
+++ calender/client/src/components/Day/index.jsx
@@ -0,0 +1,12 @@
+
+const Day = ({ date }) => {
+    console.log(date);                                                                                                                                                                                                                                                                                                                                                         
+     return (
+        <div className="flex flex-col items-center text-content justify-center h-screen">
+
+        </div>
+     );
+    };
+    
+    export default Day;
+    
